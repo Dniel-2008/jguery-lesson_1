ff16da442176059545824ed2c1dca46169439de3
Dniel-2008 <akmatbekovdaniel037@gmail.com>
2023-02-01 12:55:17 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,83 @@
+// hide()   display - none
+// show()   display - block
+// fadeIn() display - block
+// fadeOut()    display - none
+// slideDown()  display - block
+// slideUp()    display - none
+// slideToggle()   display == block => display = none 
+                // (display == none =>  display = blcok)
+
+// $(".block").fadeIn(4000)
+// $(".block").fadeOut(5000)
+
+// $(".block").slideDown(2000)
+// $(".block").slideUp(3000)
+
+// $(".block").hide(1000)
+// $(".block1").hide(2000)
+// $(".block2").hide(3000)
+// $(".block3").hide(4000)
+// $(".block4").hide(5000)
+
+// $(".block").show(6000)
+// $(".block1").show(7000)
+// $(".block2").show(8000)
+// $(".block3").show(9000)
+// $(".block4").show(10000)
+
+// $(".block").fadeOut(1000)
+// $(".block1").fadeOut(2000)
+// $(".block2").fadeOut(3000)
+// $(".block3").fadeOut(4000)
+// $(".block4").fadeOut(5000)
+
+// $(".block").fadeIn(6000)
+// $(".block1").fadeIn(7000)
+// $(".block2").fadeIn(8000)
+// $(".block3").fadeIn(9000)
+// $(".block4").fadeIn(10000)
+
+// $(".block").slideUp(1000)
+// $(".block1").slideUp(2000)
+// $(".block2").slideUp(3000)
+// $(".block3").slideUp(4000)
+// $(".block4").slideUp(5000)
+
+// $(".block").slideDown(6000)
+// $(".block1").slideDown(7000)
+// $(".block2").slideDown(8000)
+// $(".block3").slideDown(9000)
+// $(".block4").slideDown(10000)
+
+// $(".block").slideToggle(1000)
+// $(".block1").slideToggle(2000)
+// $(".block2").slideToggle(3000)
+// $(".block3").slideToggle(4000)
+// $(".block4").slideToggle(5000)
+
+// $(".block").slideToggle(6000)
+// $(".block1").slideToggle(7000)
+// $(".block2").slideToggle(8000)
+// $(".block3").slideToggle(9000)
+// $(".block4").slideToggle(10000)
+
+// function(){
+
+// }
+// ()=>{
+
+// }
+$('#btn').click(()=>{
+$(".block").slideToggle(1000)
+$(".block1").slideToggle(2000)
+$(".block2").slideToggle(3000)
+$(".block3").slideToggle(4000)
+$(".block4").slideToggle(5000)
+
+$(".block").slideToggle(6000)
+$(".block1").slideToggle(7000)
+$(".block2").slideToggle(8000)
+$(".block3").slideToggle(9000)
+$(".block4").slideToggle(10000)
+})
+
